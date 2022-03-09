8d1c5413bbecbe912d39ce30c8c097da521895d3
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-03-09 09:02:59 -0800

    upted the jason db removed the extra code

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const inventoryList = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
 
-async function getproduct(){
 
-const response = await fetch (inventoryList);
 
-const data = await response.json ();
-
-getproduct(); 
-
-console.log(data)
-
-}
